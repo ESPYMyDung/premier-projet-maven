58b316bcf7c9fa63deba69f23d1ce126b057c04e
ESPY My Dung <ngocdiepespy@sfr.fr>
2019-03-22 12:01:27 +0100

    tp maven integration librairie tierce

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -8,8 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        //System.out.println(a);        
+        //System.out.println( "Hello World!" );
+        //System.out.println(a);   
+    	
+    	String asciiArt = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt);
         
     }
 }
