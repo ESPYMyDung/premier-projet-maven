ca94084ddab03bb73a3ca71094c863dbb37ee6a4
ESPY My Dung <ngocdiepespy@sfr.fr>
2019-03-22 15:48:58 +0100

    tp maven config fichier, filtering, profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -13,8 +14,13 @@ public class App
         //System.out.println( "Hello World!" );
         //System.out.println(a);   
     	
-    	String asciiArt = FigletFont.convertOneLine("hello");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt);
+        
+        //affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
         
     }
 }
